5460a3216e438d0dd91692c469734b5a53baec4c
Armen Hovhannisyan <armen.hov@ucraft.com>
2025-01-17 12:21:49 +0400

    remove redis config

--- src/ActionMiddlewareServiceProvider.php
+++ src/ActionMiddlewareServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Uc\ActionMiddleware;
 
+use Uc\ActionMiddleware\Exceptions\ActionMiddlewareGatewayConnectionException;
 use Uc\ActionMiddleware\Gateways\ActionMiddlewareGateway\ActionMiddlewareGateway;
 use Uc\ActionMiddleware\Gateways\ActionMiddlewareGateway\ActionMiddlewareGatewayInterface;
 use Uc\ActionMiddleware\Gateways\ActionMiddlewareRunnerGateway\ActionMiddlewareRunnerGateway;
@@ -14,18 +15,10 @@ use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 class ActionMiddlewareServiceProvider extends IlluminateServiceProvider
 {
     /**
-     * @var string
-     */
-    protected string $config = __DIR__.'/../config/action-middleware.php';
-
-    /**
      * @return void
      */
     public function boot(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([$this->config => $this->app->configPath('action-middleware.php'),], 'action-middleware');
-        }
     }
 
     /**
@@ -33,13 +26,11 @@ class ActionMiddlewareServiceProvider extends IlluminateServiceProvider
      */
     public function register(): void
     {
-        $this->mergeConfigFrom($this->config, 'action-middleware');
-
         $this->app->bind(ActionMiddlewareGatewayInterface::class, function (): ActionMiddlewareGatewayInterface {
             return $this->getActionMiddlewareGateway();
         });
 
-        $this->app->singleton(ActionMiddlewareRunnerGatewayInterface::class, function () {
+        $this->app->bind(ActionMiddlewareRunnerGatewayInterface::class, function () {
             return new ActionMiddlewareRunnerGateway(new Client());
         });
     }
@@ -49,6 +40,8 @@ class ActionMiddlewareServiceProvider extends IlluminateServiceProvider
      */
     protected function getActionMiddlewareGateway(): ActionMiddlewareGatewayInterface
     {
-        return new ActionMiddlewareGateway();
+        throw new ActionMiddlewareGatewayConnectionException(
+            'Unable to create ActionMiddlewareGateway. (getActionMiddlewareGateway) method must be overridden in the extended service provider.'
+        );
     }
 }
